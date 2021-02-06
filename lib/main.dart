0229e55ec5c28cac0123e53ecec67169a45a0ad1
kinoshita0623 <kuutarousii@gmail.com>
2021-02-06 13:46:24 +0900

    fix 初期値が適切に設定されるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,23 @@ class TaskList extends StateNotifier<List<Task>> {
 
   TaskList([List<Task> tasks]) : super(tasks ?? []);
 
-  void add(String title) {
+  void create(String title) {
     final newTask = Task(title: title);
     this.state = [...this.state, newTask];
+  }
+  
+  void updateTitle({String id, String title}) {
+    this.state = [
+      for(final task in this.state)
+        if(task.id == id)
+          Task(
+            id: id,
+            title: title,
+            done: task.done
+          )
+        else
+          task
+    ];
   }
 
   void toggleDone(String id) {
@@ -48,7 +62,7 @@ class TaskList extends StateNotifier<List<Task>> {
   }
 
   void delete(String id) {
-    this.state = this.state.where((t)=> t.id != id);
+    this.state = this.state.where((t)=> t.id != id).toList();
   }
 
 
@@ -170,7 +184,19 @@ class TaskListComponent extends HookWidget {
               ];
             },
             onSelected: (int value){
-
+              switch(value){
+                case 1:
+                  Navigator.pushNamed(context, "/create", arguments: TaskEditorPageArgs(taskId: tasks[index].id));
+                  break;
+                case 2:
+                  showDialog(
+                    context: context,
+                    builder: (_){
+                      return AlertConfirmTaskDeletion(taskId: tasks[index].id);
+                    }
+                  );
+                  break;
+              }
             },
           ),
         );
@@ -179,32 +205,95 @@ class TaskListComponent extends HookWidget {
   }
 }
 
+class TaskEditorPageArgs {
+  final String taskId;
+  TaskEditorPageArgs({this.taskId});
+}
 
 class TaskEditorPage extends HookWidget {
 
-  final taskTitleController = TextEditingController();
+  //final taskTitleController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final taskList = useProvider(taskListProvider);
+    final tasks = useProvider(taskListProvider.state);
+
+    final TaskEditorPageArgs args = ModalRoute.of(context).settings.arguments;
+    final taskId = useState<String>(args?.taskId);
+    final text = useState<String>();
+    final task = useState<Task>();
+
+    if(args?.taskId != null){
+      task.value = tasks.firstWhere((element) => element.id == args.taskId);
+
+      text.value = task.value.title;
+      //taskTitleEditingController.text = text.value;
+    }
+    final taskTitleEditingController = useTextEditingController(text: text.value ?? "");
 
     return Scaffold(
-      appBar: AppBar(title: Text("作成"),),
+      appBar: AppBar(title: buildTitle(taskId.value)),
       body: Column(
         children: [
 
           TextField(
-            controller: taskTitleController,
+            controller: taskTitleEditingController,
             onChanged: (e){
+              text.value = e;
             },
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(onPressed: (){
-        taskList.add(taskTitleController.text);
-        Navigator.pop(context);
-      }, label: Text("作成"), icon: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          if(task.value == null){
+            taskList.create(taskTitleEditingController.text);
+          }else{
+            taskList.updateTitle(id: task.value.id, title: taskTitleEditingController.text);
+          }
+          Navigator.pop(context);
+        },
+        label: Text("保存"),
+        icon: Icon(Icons.add)
+      ),
+    );
+  }
+
+  Widget buildTitle(String taskId) {
+    if(taskId == null){
+      return Text("作成");
+    }else{
+      return Text("編集");
+    }
+  }
+
+}
+
+class AlertConfirmTaskDeletion extends HookWidget {
+  AlertConfirmTaskDeletion({this.taskId});
+  final String taskId;
+
+  @override
+  Widget build(BuildContext context) {
+    final tl = useProvider(taskListProvider);
+
+    return AlertDialog(
+      title: Text("削除の確認"),
+      content: Text("タスクを削除します。"),
+      actions: [
+        FlatButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          child: Text("キャンセル")
+        ),
+        FlatButton(
+          onPressed: (){
+            tl.delete(taskId);
+            Navigator.pop(context);
+          }, child: Text("削除する"))
+      ]
     );
   }
 }
-
